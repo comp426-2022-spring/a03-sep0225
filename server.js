fe3d415e993a6b8fd21cd69dd33465fb803a109b
@sep0225 <sep0225@live.unc.edu>
2022-04-30 10:36:19 -0400

    updated server.js pt 2

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-// import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs"
-// import { createRequire } from 'module'
-// const require = createRequire(import.meta.url)
-
 // Require Express.js
 const express = require('express')
 const app = express()
 
-const args = require('minimist')(process.argv.slice(2))
+const min = require('minimist')
+const args = min(process.argv.slice(2))
+
 args['port']
 const port = args.port || process.env.PORT || 5000
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
-});
-
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
 });
 
 app.get('/app/', (req, res) => {
@@ -53,6 +46,11 @@ app.get('/app/flip/call/tails', (req, res) => {
     res.statusCode = 200
     res.json(tails)
 })
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
 
 /** Simple coin flip
  * 
