9bbfead85392d57b62de7898950a0e7a11ed2907
@sep0225 <sep0225@live.unc.edu>
2022-04-30 14:31:23 -0400

    blahh

--- server.js
+++ server.js
@@ -3,95 +3,60 @@ const app = express()
 
 const args = require('minimist')(process.argv.slice(2))
 args['port']
-const port = args.port || 5555
+const port = args.port || process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
-app.get('/app/', (req, res) => {
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
-});
-
 function coinFlip() {
-  // return Math.random() < 0.6 ? ("heads") : ("tails")
-  var ran = Math.random();
-    if (ran < 0.5) {
-        return "heads";
-    }
-    else {
-        return "tails";
-    }
+  return Math.random() < 0.6 ? ("heads") : ("tails")
 }
 
 function coinFlips(flips) {
-  // const arr = [];
-  // for (let i = 0; i < flips; i++) {
-  //   arr[i] = coinFlip();
-  // }
-  // return arr;
-  if (flips < 0 || flips == '' || flips == null) {
-    console.log('Error: no input');
-  } else {
-    var list = [];
-    for(var i = 0; i < flips; i++) {
-    list.push(coinFlip());
-    }
-    return list;
-    }
+  const arr = [];
+  for (let i = 0; i < flips; i++) {
+    arr[i] = coinFlip();
+  }
+  return arr;
 }
 
 function countFlips(array) {
-  // let h_amt = 0;
-  // let t_amt = 0;
-  // for (let i = 0; i < array.length; i++) {
-  //   if (array[i] == "heads") {
-  //     h_amt += 1;
-  //   } else {
-  //     t_amt += 1;
-  //   }
-  // }
-  // if (h_amt == 0) {
-  //   return "{ tails: " + t_amt + " }";
-  // }
-  // if (t_amt == 0) {
-  //   return "{ heads: " + h_amt + " }";
-  // }
-  // return "{ heads: " + h_amt + ", tails: " + t_amt + " }";
-  var heads = 0;
-    var tails = 0;
-    for(var i = 0; array.length > i; i++) {
-        if (array[i] == "heads") {
-            heads++;
-        }
-    else {
-            tails++
-        }
+  let h_amt = 0;
+  let t_amt = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == "heads") {
+      h_amt += 1;
+    } else {
+      t_amt += 1;
+    }
   }
-  return {'heads' : heads, 'tails' : tails}
+  if (h_amt == 0) {
+    return "{ tails: " + t_amt + " }";
+  }
+  if (t_amt == 0) {
+    return "{ heads: " + h_amt + " }";
+  }
+  return "{ heads: " + h_amt + ", tails: " + t_amt + " }"; 
 }
 
 function flipACoin(call) {
-  // let flip = coinFlip();
-  // let result = "";
-  // if (call == flip) {
-  //   result = "win";
-  // } else {
-  //   result = "lose";
-  // }
-  // return "{ call: '" + call + "', flip: '" + flip + "', result: '" + result + "' }";
-  var flip = coinFlip();
-  var result = ""
+  let flip = coinFlip();
+  let result = "";
   if (call == flip) {
-    result = "win"
+    result = "win";
   } else {
-    result = "lose"
+    result = "lose";
   }
-  return {"call": call, "flip": flip, "result": result};
+  return "{ call: '" + call + "', flip: '" + flip + "', result: '" + result + "' }";
 }
+
+app.get('/app/', (req, res) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+  res.end(res.statusCode+ ' ' +res.statusMessage)
+});
 
 app.get('/app/flip/', (req, res) => {
     const flip = coinFlip()
